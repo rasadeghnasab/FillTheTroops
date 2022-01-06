87f25972e43aac9e9d24de0ebd2369fe5799607a
Ramin <rasadeghnasab@gmail.com>
2022-01-06 18:58:01 +0330

    - Remove extra blank lines

--- app/http/controllers/troopController.js
+++ app/http/controllers/troopController.js
@@ -2,8 +2,6 @@ const solution = config.useFakeSomething ?
     require('./FakeSomething').FakeSomething :
     require('./RealSomething').RealSomething;
 
-
-
 exports.suggest_troops = async function(request, response) {
     response.status(200).send('NOT IMPLEMENTED: Troops suggestion');
 };
