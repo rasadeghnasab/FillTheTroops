b8eea470ebeb648cead3b28007828ca0dbb81075
Ramin <rasadeghnasab@gmail.com>
2022-01-08 23:36:45 +0330

    Add troop suggestion functionality Add coverage

--- app/http/controllers/troopController.js
+++ app/http/controllers/troopController.js
@@ -6,5 +6,21 @@ const utilities = require('../../modules/utilities');
 const solution01 = require('../../modules/solution-01');
 
 exports.suggest_troops = async function(request, response) {
-    response.status(200).send(solution01(request.query.armyCount));
+    const armyCount = request.query.armyCount;
+
+    if(armyCount != parseInt(armyCount)) {
+        return response.status(421).send({
+            message: 'armyCount should be an integer'
+        });
+    }
+
+    if(armyCount < 1) {
+        return response.status(421).send({message: 'armyCount should be greater than zero'})
+    }
+
+    if(armyCount < 3) {
+        return response.status(421).send({message: 'Army should not be less than troops count'})
+    }
+
+    response.status(200).send(solution01(armyCount));
 };
--- tests/integration/troops/suggest.test.js
+++ tests/integration/troops/suggest.test.js
@@ -11,64 +11,68 @@ describe("/api/troops/suggest", () => {
         server.close();
     });
 
-    describe("GET /", () => {
+    describe("the route exists", () => {
         it("should not return 404 (exists)", async () => {
             const response = await request(server).get("/api/troops/suggest");
             expect(response.status).not.toBe(404);
             expect(response.type).toEqual('application/json')
         });
     });
-    describe("GET /", () => {
+    describe("input and output are equal", () => {
         it("the army members count should be equal to the entered input number", async () => {
             const armyCounts = [3, 50, 100, 167, 200, 1000, 1000000];
 
             for (const expectedArmyCount of armyCounts) {
                 const response = await request(server).get("/api/troops/suggest").query({armyCount: expectedArmyCount});
                 const actualArmyCount = Object.values(response.body).reduce((a, b) => a + b, 0)
-                console.log(actualArmyCount)
-                expect(expectedArmyCount).not.toBe(actualArmyCount);
+
+                expect(expectedArmyCount).toEqual(actualArmyCount);
             }
         });
     });
-
-    describe("GET /", () => {
-        it("input should be an integer greater", async () => {
-            const response = await request(server).get("/api/troops/suggest");
-            expect(response.status).toBe(401);
-            expect(response.body).toBe('Input should be an integer');
-        });
-        it("input should be greater than zero", async () => {
-            let response = await request(server).get("/api/troops/suggest").query({armyCount: 0});
-            expect(response.status).toBe(401);
-            expect(response.body).toBe('Input should be greater than zero');
-
-            response = await request(server).get("/api/troops/suggest").query({armyCount: -1});
-            expect(response.status).toBe(401);
-            expect(response.body).toBe('Input should be greater than zero');
-        });
-        it("input should not be less than troops types count", async () => {
-            const response = await request(server).get("/api/troops/suggest");
-            expect(response.status).not.toBe(404);
-        });
-    });
-
-    describe("GET /", () => {
-        const rounds = 100;
-        const input = 167;
+    describe("unique result", () => {
+        const rounds = 200;
+        const input = 5000;
         it(`result should be unique each time for same input in ${rounds} rounds`, async () => {
             const results = [];
             for(let round = 0; round < rounds; round++) {
                 const response = await request(server).get("/api/troops/suggest").query({armyCount: input});
                 results.push(JSON.stringify(response.body));
             }
-            // console.log(results)
 
             // if there is no duplicate the result array should have 100 elements after executing the _.uniq function
-            expect(_.uniq(results)).toHaveLength(100);
+            expect(_.uniq(results)).toHaveLength(rounds);
         });
         it("input should not be less than troops types count", async () => {
-            const response = await request(server).get("/api/troops/suggest");
-            expect(response.status).not.toBe(404);
+            const response = await request(server).get("/api/troops/suggest").query({armyCount: input});
+
+            expect(Object.keys(response.body).length).toEqual(3);
+        });
+    });
+    describe("input validation", () => {
+        const armyCountInput = 200;
+        it("input should be an integer", async () => {
+            const response = await request(server).get("/api/troops/suggest").query({armyCount: 'not an integer'});
+            expect(response.status).toBe(421);
+            expect(response.body.message).toBe('armyCount should be an integer');
+        });
+        it("input should be greater than zero", async () => {
+            const testSamples = [-10, -1, 0];
+
+            for (const armyCountInput of testSamples) {
+                let response = await request(server).get("/api/troops/suggest").query({armyCount: armyCountInput});
+                expect(response.status).toBe(421);
+                expect(response.body.message).toBe('armyCount should be greater than zero');
+            }
+        });
+        it("input should not be less than troops types count", async () => {
+            const testSamples = [1, 2];
+
+            for (const armyCountInput of testSamples) {
+                const response = await request(server).get("/api/troops/suggest").query({armyCount: armyCountInput});
+                expect(response.status).toEqual(421);
+                expect(response.body.message).toEqual('Army should not be less than troops count')
+            }
         });
     });
 });
