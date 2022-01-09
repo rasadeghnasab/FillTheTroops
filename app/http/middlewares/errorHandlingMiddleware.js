f1bfb447b26b66e42af960a4eb2bd81488c07cb9
Ramin <rasadeghnasab@gmail.com>
2022-01-09 10:47:56 +0330

    - Remove extra lines

--- app/http/middlewares/errorHandlingMiddleware.js
+++ app/http/middlewares/errorHandlingMiddleware.js
@@ -3,6 +3,5 @@ const winston = require("winston");
 module.exports = (error, request, response, next) => {
     winston.error("=>", error);
 
-    console.log(error.message);
     return response.status(error.statusCode).send({message: error.message, statusCode: error.statusCode});
 };
